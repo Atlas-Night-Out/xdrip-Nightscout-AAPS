e00e1339ced1cfb358f4b3adf93edd059fcdda3a
thediabeticway <support@thediabeticway.co.uk>
2025-01-13 21:22:29 +0000

    Deployed 9fe3a388 with MkDocs version: 1.5.3

---
+++ css/javascripts/extra.js
@@ -0,0 +1,19 @@
+extra_javascript:
+  - javascripts/extra.js
+  
+  theme:
+  palette:
+
+    # Palette toggle for light mode
+    - media: "(prefers-color-scheme: light)"
+      scheme: black
+      toggle:
+        icon: material/brightness-7
+        name: Switch to dark mode
+
+    # Palette toggle for dark mode
+    - media: "(prefers-color-scheme: dark)"
+      scheme: slate
+      toggle:
+        icon: material/brightness-4
+        name: Switch to light mode
